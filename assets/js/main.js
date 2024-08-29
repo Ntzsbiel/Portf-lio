6343ed2493b2a29726c88acb7370c90ba8c57e89
Ntzsbiel <gabrielsilva.ilha186@gmail.com>
2024-08-28 23:04:31 -0300

    Finish day, working to make dinamic profile

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+function importProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const linkedin = document.getElementById('profile.linkedin')
+    linkedin.innerText = profileData.linkedin
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+}
+
+
+function importSoftSkills(profileData){
+    const softskills = document.getElementById('profile.skills.softSkills')
+
+    softskills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+    
+}
+(async () => {
+    const profileData = await fetchProfileData()
+    importProfileInfo(profileData)
+    importSoftSkills(profileData)
+})()
