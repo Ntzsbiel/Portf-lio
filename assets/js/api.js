f808ffda095d6f758f758ce565c4cbd32bb79c46
Ntzsbiel <gabrielsilva.ilha186@gmail.com>
2024-08-29 21:24:17 -0300

    Testing

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 //Função da api que retorna as informações 
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Ntzsbiel/Portfolio/masterassets/img/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/Ntzsbiel/Portfolio/master/assets/img/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
