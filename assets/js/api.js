60a296cdf637db74e60d4f8f539d1eb0bd0ee6eb
Ntzsbiel <gabrielsilva.ilha186@gmail.com>
2025-01-18 23:05:04 -0300

    teste

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,5 +3,10 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/Ntzsbiel/Portfolio/master/assets/img/data/profile.json'
     const fetching = await fetch(url)
-    return await fetching.json()
+    const profileData = await fetching.json()
+
+    profileData.forEach(icon => {
+        console.log(icon.logo);  // Verifique se a URL do SVG está correta no console
+    })
+    return profileData
 }
